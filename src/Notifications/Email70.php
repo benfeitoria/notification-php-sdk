f46d6d5a4c128760297a4f67de218e94090230d9
Phil <philipe.lagreca@gmail.com>
2019-12-13 14:44:45 -0300

    Email70 requiredFields

--- src/Notifications/Email70.php
+++ src/Notifications/Email70.php
@@ -16,15 +16,10 @@ class Email70 extends BaseNotification
     public function getRequiredFields(): array
     {
         return [
+            "projects_id",
             "projects_title",
-            "projects_transfered_value",
-            "projects_short_url",
-            "projects_bank_accounts_bank",
-            "projects_bank_accounts_office",
-            "projects_bank_accounts_account",
-            "projects_bank_accounts_document_type",
-            "projects_bank_accounts_document_number",
-            "projects_bank_accounts_legal_name"   
+            "projects_pagarme_recipient_id",
+            "projects_withdrawl_value"
         ];
     }
 }
