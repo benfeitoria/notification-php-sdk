1e8daef9f78553afbdaae21a0c51b8eeeb2b889f
André Assis <andretefras@gmail.com>
2020-06-25 18:54:09 -0300

    Cleanup of exceptions

--- src/Exceptions/NotificationMissingRequiredFieldsException.php
+++ src/Exceptions/NotificationMissingRequiredFieldsException.php
@@ -1,18 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: j
- * Date: 24/07/19
- * Time: 17:17
- */
 
 namespace Benfeitoria\Notification\Exceptions;
 
-
 class NotificationMissingRequiredFieldsException extends \Exception
 {
-    public function __construct(string $message = "Notification missing required fields", int $code = 401, \Throwable $previous = null)
-    {
+    public function __construct(
+        string $message = "Notification missing required fields",
+        int $code = 401,
+        \Throwable $previous = null
+    ) {
         parent::__construct($message, $code, $previous);
     }
 }
