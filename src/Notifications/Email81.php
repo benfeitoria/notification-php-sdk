a15a7fc2546efa1ce5657f4d4c620565f237e053
phil <philipe.lagreca@gmail.com>
2020-07-30 15:54:00 -0300

    New Email81

---
+++ src/Notifications/Email81.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Benfeitoria\Notification\Notifications;
+
+class Email81 extends BaseNotification
+{
+    public $description = "E-mail 81 - Uma campanha publicada enviou uma mensagem! =D";
+
+    /**
+     * @return string
+     */
+    public function getNotification(): string
+    {
+        return "email/81";
+    }
+
+    /**
+     * @return array
+     */
+    public function getRequiredFields(): array
+    {
+        return [
+            "owner_email",
+            "projects_title",
+            "projects_id",
+            "messages_content"
+        ];
+    }
+}
