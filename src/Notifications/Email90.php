fdf8893c61c5ef5495c53e39726924024d0f74a1
lucas-cn <lucascorreia177@yahoo.com.br>
2021-07-08 15:02:58 -0300

    Add template email for send pix qrcode and generate a new

---
+++ src/Notifications/Email90.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Benfeitoria\Notification\Notifications;
+
+class Email90 extends BaseNotification
+{
+    public $description = "E-mail 90 - Disparado 1 dia após a geração do codigo do pix SE o pagamento não foi identificado..";
+
+    /**
+     * @return string
+     */
+    public function getNotification(): string
+    {
+        return "email/90";
+    }
+
+    /**
+     * @return array
+     */
+    public function getRequiredFields(): array
+    {
+        return [
+            "contributor_name",
+            "contributor_email",
+            "projects_title",
+            "projects_short_url",
+            "generate_pix_qr_code_url"
+        ];
+    }
+}
