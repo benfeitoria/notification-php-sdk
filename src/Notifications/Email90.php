721596e91402d4c5ebefb29688bfb5a0b8b1d490
lucas-cn <lucascorreia177@yahoo.com.br>
2021-07-12 15:03:04 -0300

    Change mail90 param

--- src/Notifications/Email90.php
+++ src/Notifications/Email90.php
@@ -24,7 +24,7 @@ class Email90 extends BaseNotification
             "contributor_email",
             "projects_title",
             "projects_short_url",
-            "refresh_pix_qr_code_url"
+            "pix_qr_code"
         ];
     }
 }
