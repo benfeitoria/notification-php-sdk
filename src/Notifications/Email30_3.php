b4e27c4dc64339f4103573ef31a6ca3a60b87648
Lucas <lucascorreia177@yahoo.com.br>
2023-03-29 19:47:04 -0300

    Add email for contribution by chapix

---
+++ src/Notifications/Email30_3.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Benfeitoria\Notification\Notifications;
+
+
+class Email30_3 extends BaseNotification
+{
+    public $description = "E-mail 30-3  - E-mail a ser disparado se a pessoa colaborou com uma campanha sem meta.";
+
+    /**
+     * @return string
+     */
+    public function getNotification(): string
+    {
+        return "email/30_3";
+    }
+
+    /**
+     * @return array
+     */
+    public function getRequiredFields(): array
+    {
+        return [
+            "contributor_name",
+            "contributor_email",
+            "projects_title",
+            "projects_short_url",
+            "contributions_value",
+            "owner_email"
+        ];
+    }
+}
